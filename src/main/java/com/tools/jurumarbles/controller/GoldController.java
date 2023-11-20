fb70ccfd06c7e482422ba56a827c503f2acb9a49
Yeon73 <yunalove7373@gmail.com>
2023-11-21 04:17:31 +0900

    Feat: 황금 열쇠 가져오기
    !주의: 아마 버전이 다른 것 땜에 실행을 못해봄. 오류 있을 수 있음

---
+++ src/main/java/com/tools/jurumarbles/controller/GoldController.java
@@ -0,0 +1,43 @@
+package com.tools.jurumarbles.controller;
+
+import com.tools.jurumarbles.model.GoldEntity;
+import com.tools.jurumarbles.repository.GoldRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class GoldController {
+
+    private final GoldRepository goldRepository;
+    @Autowired
+    public GoldController(GoldRepository goldRepository) {
+        this.goldRepository = goldRepository;
+    }
+    @GetMapping("/api/v1/goldKey")
+    public String getRandomGold(Model model) {
+        // Get all entities from the table
+        List<GoldEntity> allEntities = goldRepository.findAll();
+
+        // Check if there are entities in the table
+        if(allEntities.isEmpty()) {
+            model.addAttribute("errorMessage", "No entities found in the 'gold_entity'");
+        } else {
+            // Get a random index
+            int randomIndex = new Random().nextInt(allEntities.size());
+
+            // Get the randomly selected entity
+            GoldEntity randomEntity = allEntities.get(randomIndex);
+
+            // Add the entity to the model for rendering in the view
+            model.addAttribute("randomEntity", randomEntity);
+        }
+
+        // Return the name of the Thymeleaf template
+        return "goldKey";
+    }
+}
